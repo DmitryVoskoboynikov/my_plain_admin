120576d8ed719b65b12af672126181d78f63bedf
DmitryVoskoboynikov <dmitry.voskoboynikoff@gmail.com>
2025-07-08 17:35:47 +0300

    add to file assets3/js/bootstrap.bundle.js consts: namespaceRegex, stripNameRegex, stripUidRegex, eventRegistry, customEvents, nativeEvents and let: uidEvent

--- assets3/js/bootstrap.bundle.js
+++ assets3/js/bootstrap.bundle.js
@@ -303,5 +303,29 @@
         return list[Math.max(0, Math.min(index, listLength - 1))];
     };
 
+    /**
+     * --------------------------------------------------------------------------
+     * Bootstrap dom/event-handler.js
+     * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+     * --------------------------------------------------------------------------
+     */
+
+
+    /**
+     * Constants
+     */
+
+    const namespaceRegex = /[^.]*(?=\..*)\.|.*/;
+    const stripNameRegex = /\..*/;
+    const stripUidRegex = /::\d+$/;
+    const eventRegistry = {}; // Events storage
+    let uidEvent = 1;
+    const customEvents = {
+        mouseenter: 'mouseover',
+        mouseleave: 'mouseout'
+    };
+    const nativeEvents = new Set(['click', 'dblclick', 'mouseup', 'mousedown', 'contextmenu', 'mousewheel', 'DOMMouseScroll', 'mouseover', 'mouseout', 'mousemove', 'selectstart', 'selectend', 'keydown', 'keypress', 'keyup', 'orientationchange', 'touchstart', 'touchmove', 'touchend', 'touchcancel', 'pointerdown', 'pointermove', 'pointerup', 'pointerleave', 'pointercancel', 'gesturestart', 'gesturechange', 'gestureend', 'focus', 'blur', 'change', 'reset', 'select', 'submit', 'focusin', 'focusout', 'load', 'unload', 'beforeunload', 'resize', 'move', 'DOMContentLoaded', 'readystatechange', 'error', 'abort', 'scroll']);
+
+    
 
 }));
